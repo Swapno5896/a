0f4314a772c9301a65ab4eb708adda5946989bb2
Tareq Hassan <tareqhassan2014@gmail.com>
2021-05-09 23:59:14 +0600

    from Tareq Hassan

--- a.js
+++ a.js
@@ -1,3 +1,10 @@
 const a = 3;
 const b = 4;
 console.log(a,b);
+
+const number = [1,2,3,4,5,6,7,8,9,11,2,2,,3,3435,667,8,86,5]
+
+for (let i = 0; i < number.length; i++) {
+    const num = number[i];
+    console.log(num);
+}
